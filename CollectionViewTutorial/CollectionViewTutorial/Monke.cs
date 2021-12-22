36cb1bf62bbac6bbe04ab1fb00bc8ac13fbe92b5
daddy <daddy@DESKTOP-C37T556>
2021-12-22 23:49:23 +0300

    Add project files.

---
+++ CollectionViewTutorial/CollectionViewTutorial/Monke.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionViewTutorial
+{
+    public class Monke
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
